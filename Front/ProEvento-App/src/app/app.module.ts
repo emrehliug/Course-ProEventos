34cf36aeee6a1450a6f275078a6cbcd312fee06d
emrehliug <guigui410@gmail.com>
2023-03-08 08:41:05 -0300

    Layout responsivo + Forms

--- Front/ProEvento-App/src/app/app.module.ts
+++ Front/ProEvento-App/src/app/app.module.ts
@@ -19,12 +19,15 @@ import { EventosComponent } from './components/eventos/eventos.component';
 import { TituloComponent } from './components/shared/titulo/titulo.component';
 import { ContatosComponent } from './components/contatos/contatos.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { PerfilComponent } from './components/perfil/perfil.component';
+import { PerfilComponent } from './components/user/perfil/perfil.component';
 
 import { EventoService } from './service/evento.service';
 import { DateTimeFormatPipe } from './helpers/DateTimeFormat.pipe';
 import { EventoDetalheComponent } from './components/eventos/evento-detalhe/evento-detalhe.component';
 import { EventoListaComponent } from './components/eventos/evento-lista/evento-lista.component';
+import { UserComponent } from './components/user/user.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { RegistrationComponent } from './components/user/registration/registration.component';
 
 
 @NgModule({
@@ -39,7 +42,10 @@ import { EventoListaComponent } from './components/eventos/evento-lista/evento-l
       DashboardComponent,
       PerfilComponent,
       EventoDetalheComponent,
-      EventoListaComponent
+      EventoListaComponent,
+      UserComponent,
+      LoginComponent,
+      RegistrationComponent
    ],
   imports: [
     BrowserModule,
